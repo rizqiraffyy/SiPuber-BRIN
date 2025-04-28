655e074dd5ecc695244b812ff23971172212d410
hanifazhar <azharhanif04@gmail.com>
2025-04-28 14:56:25 +0700

    ilangin readme

---
+++ .next/static/chunks/app_page_tsx_8fa74d2a._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_2cc5a98e._.js",
+  "static/chunks/_6c514501._.js"
+],
+    source: "dynamic"
+});
