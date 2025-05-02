c10f4d04e3d3e03f1ad72767b27946ba363af17d
Candika <candikadwihandaru@mail.ugm.ac.id>
2025-05-02 13:55:12 +0700

    update server

--- server.ts
+++ server.ts
@@ -1,33 +1,31 @@
-import express from 'express';
+// server.mjs or server.ts (if using TypeScript)
+
+import { createServer } from "node:http";
 import next from 'next';
-// import axios from 'axios';
-import http from 'http';
-import { Server } from 'socket.io';
+// import http from 'http';
+import { Server as SocketIOServer } from 'socket.io';
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
 
-app.prepare().then(async () => {
-    const server = express();
-    const httpServer = http.createServer(server);
-    const io = new Server(httpServer);
+app.prepare().then(() => {
+  const httpServer = createServer(handler);
+  const io = new SocketIOServer(httpServer); // renamed for clarity
 
-    io.on('connection', (socket) => {
-        console.log('Client connected');
+  io.on('connection', (socket) => {
+    console.log('Client server connected '+socket.id);
 
-        socket.on('iot-update', (data) => {
-            console.log('Recieved from API ::', data);
-            io.emit('iot-update', data);
-        });
+    socket.on('iot-update', (data) => {
+      console.log('Data thru server ::', data);
+      io.emit('iot-update', data);
     });
+  });
 
-    server.all('*', (req, res) => {
-        return handle(req, res);
-    });
-
-    const PORT = process.env.PORT || 3000;
-    httpServer.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    });
+  const PORT = process.env.PORT || 3000;
+  httpServer.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 });
